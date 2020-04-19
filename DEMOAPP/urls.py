d68ff4e543136cc972c105b253add06655db3b27
Shivlal Sharma <40575397+sshiv5768@users.noreply.github.com>
2020-04-19 18:04:40 +0530

    Update urls.py

--- DEMOAPP/urls.py
+++ DEMOAPP/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name='home-page'),
     path('about/', views.about_us, name='about'),
@@ -9,7 +10,10 @@ urlpatterns = [
     path('login/', views.loginuser, name='login'),
     path('logout/', views.logoutuser, name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('submit_health/', views.submit_health, name='submit_health')
+    path('submit_health/', views.submit_health, name='submit_health'),
+    path('submit_photo/', views.submit_photo, name='submit_photo')
 
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
